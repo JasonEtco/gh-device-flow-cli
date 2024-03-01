9f95a87a1dfc058612903ac0edf805a03d7d5fe3
Jason Etcovitch <jasonetco@github.com>
2024-02-29 21:51:26 -0500

    Init to winit

---
+++ index.mjs
@@ -0,0 +1,31 @@
+import { createOAuthDeviceAuth } from "@octokit/auth-oauth-device";
+
+// USAGE:
+// node index.mjs <client_id>
+
+async function main() {
+  const clientID = process.argv[2];
+  if (!clientID) {
+    throw new Error("client_id required");
+  }
+  if (!clientID.startsWith("Iv1.")) {
+    throw new Error(`invalid client_id: ${clientID}`);
+  }
+
+  const auth = createOAuthDeviceAuth({
+    clientType: "github-app",
+    clientId: clientID,
+    onVerification(verification) {
+      console.log("Open %s", verification.verification_uri);
+      console.log("Enter code: %s", verification.user_code);
+    },
+  });
+  
+  const tokenAuthentication = await auth({
+    type: "oauth",
+  });
+
+  console.log(tokenAuthentication)
+}
+
+main().catch(console.error);
